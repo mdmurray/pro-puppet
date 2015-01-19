e8ed9a2a6d3dd15a1bcc07ac493c0c76b42f75f5
Mark Murray <md.murray@outlook.com>
2015-01-19 23:37:33 +0000

    added ssh module + install.pp config.pp service.pp

---
+++ ssh/manifests/service.pp
@@ -0,0 +1,10 @@
+class ssh::service {
+  service { "sshd":
+    ensure	=> running,
+    hasstatus	=> true,
+    hasrestart  => true,
+    enable	=> true,
+    require	=> Class["ssh::config"],
+    }
+}
+
